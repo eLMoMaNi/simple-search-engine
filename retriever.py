d4e86ba4d9d882290116abe9aeeb20f781000eea
eLMoMaNi <ahmad.mom.20000@gmail.com>
2021-05-22 23:41:54 +0300

    basic Retriever class

---
+++ retriever.py
@@ -0,0 +1,25 @@
+from indexer import preprocess 
+
+class Retriever:
+    def __init__(self, schema_file) -> None:
+        self.__schema_file = schema_file
+
+    def create_query_vector(self, text):
+        raise NotImplementedError
+
+    def create_doc_vector(self, doc_id, query_text):
+        raise NotImplementedError
+
+    def cos_similarity(self):
+        raise NotImplementedError
+
+    def query(self, text, k=100, print_bench=False):
+        """make a query from plain text and return top `k` relevant docs.
+        `print_bench` prints the benchmarks: `Accuracy, F1 , Precision, Recall`
+
+        Args:
+            text (str): plain text query
+            k (int, optional): how many docs to retriev. Defaults to 100.
+            print_bench (bool, optional): a flag to print benchmarks. Defaults to False.
+        """
+        raise NotImplementedError
