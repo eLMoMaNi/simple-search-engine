d2b4ad85aabf6abfc81e23370626f5da3efe0d97
eLMoMaNi <ahmad.mom.20000@gmail.com>
2021-05-23 00:23:06 +0300

    implement create vector for query and doc

--- retriever.py
+++ retriever.py
@@ -1,14 +1,60 @@
-from indexer import preprocess 
+import json
+import math
+from indexer import preprocess
+
 
 class Retriever:
     def __init__(self, schema_file) -> None:
         self.__schema_file = schema_file
+        self.schema = json.load(schema_file)
 
     def create_query_vector(self, query_list):
-        raise NotImplementedError
+        """creates a normalized query vector using `ltc` weighting.
+
+        Args:
+            query_list (list): the query as list of strings.
+        """
+        query_vector = []
+
+        for term in query_list:
+            tf = query_list.count(term)
+            idf = self.schema[term]["idf"]
+
+            # using ltc weighting
+            l = 1+math.log10(tf)
+            t = math.log10(idf)
+
+            tf_idf = l*t
+            query_vector.append(tf_idf)
+
+        norm = math.sqrt(sum(i*i for i in query_vector))
+
+        normalized_vector = [i/norm for i in query_vector]
+        return normalized_vector
 
     def create_doc_vector(self, doc_id, query_list):
-        raise NotImplementedError
+        """creates a normalized doc vector using `lnc` weighting.
+
+        Args:
+            doc_id (str): document id
+            query_list (list): the doc as list of strings.
+        """
+
+        doc_vector = []
+        for term in query_list:
+            if doc_id in self.schema[term]:
+                tf = self.schema[term][doc_id]
+
+                l = 1+math.log10(tf)
+                doc_vector.append(l)
+            else:
+                doc_vector.append(0)
+        
+        norm = math.sqrt(sum(i*i for i in doc_vector))
+
+        normalized_vector = [i/norm for i in doc_vector]
+        return normalized_vector
+        
 
     def cos_similarity(self):
         raise NotImplementedError
