89adc8b0e7eca66e9e469cada0b9011fe80e5335
eLMoMaNi <ahmad.mom.20000@gmail.com>
2021-05-23 14:53:17 +0300

    implement benchmark for query

--- retriever.py
+++ retriever.py
@@ -7,7 +7,7 @@ from indexer import Indexer
 class Retriever:
     def __init__(self, schema_file) -> None:
         self.__schema_file = schema_file
-        self.schema = json.load(open(schema_file,"r"))
+        self.schema = json.load(open(schema_file, "r"))
 
     def create_query_vector(self, query_list):
         """creates a normalized query vector using `ltc` weighting.
@@ -58,7 +58,7 @@ class Retriever:
     def cos_similarity(self, a, b):
 
         if len(a) != len(b):
-            print(a,b)
+            print(a, b)
             raise Exception("a and b must have the same lenght")
         sum = 0
         for i in range(len(a)):
@@ -66,14 +66,16 @@ class Retriever:
 
         return sum
 
-    def query(self, text, k=100, print_bench=False):
+    def query(self, text, k=100, get_bench=False, relevance_docs=[]):
         """make a query from plain text and return top `k` relevant docs.
-        `print_bench` prints the benchmarks: `Accuracy, F1 , Precision, Recall`
+        `get_bench` returns the benchmarks as dict: `Accuracy, F1 , Precision, Recall`
 
         Args:
             text (str): plain text query
             k (int, optional): how many docs to retriev. Defaults to 100.
-            print_bench (bool, optional): a flag to print benchmarks. Defaults to False.
+            get_bench (bool, optional): a flag to print benchmarks. Defaults to False.
+            relevance_docs (list, optional): relevance docs to calculate benchmarks, must be passed 
+            when `get_bench` is true
         """
 
         # preprocess plain text
@@ -86,20 +88,47 @@ class Retriever:
 
         for token in tokens:
             for doc in self.schema[token]:
-                if doc in doc_vectors or doc=="idf":  # if vector already calculated
+                if doc in doc_vectors or doc == "idf":  # if vector already calculated
                     continue
                 else:
                     doc_vectors[doc] = self.create_doc_vector(doc, tokens)
 
-        doc_scores={}
+        doc_scores = {}
         for doc in doc_vectors:
-            doc_scores[doc] = self.cos_similarity(query_vector,doc_vectors[doc])
-        
+            doc_scores[doc] = self.cos_similarity(
+                query_vector, doc_vectors[doc])
 
-        # TODO
-        if print_bench:
-            raise NotImplementedError
-        
-        #sort docs by thier scores
-        sorted_docs= sorted(doc_scores.items(), key=operator.itemgetter(1))
-        return [i[0] for i in sorted_docs[-k:]]
+        # sort docs by thier scores
+        sorted_docs = sorted(doc_scores.items(), key=operator.itemgetter(1))
+        ret = [i[0] for i in sorted_docs[-k:]]
+
+        if get_bench:
+            if not relevance_docs:
+                raise Exception(
+                    "print_bench is true but relevance_docs is empty")
+
+            tp, fp, fn, tn = 0, 0, 0, 0
+            for ret_doc in ret:
+                if ret_doc in relevance_docs:
+                    tp += 1
+                else:
+                    fp += 1
+            for rel_doc in relevance_docs:
+                if rel_doc not in ret:
+                    fn += 1
+            # TODO change this to getter value
+            tn = 1400-(tp+fp+fn)
+            accuracy = (tp+tn)/(tp+tn+fp+fn)
+            precision = tp/(tp+fp)
+            recall = tp/(tp+fn)
+            f1_score = 0 if precision + \
+                recall == 0 else (2*precision*recall)/(precision+recall)
+            return [ret, {
+                "accuracy": round(accuracy,4),
+                "precision": round(precision,4),
+                "recall": round(recall,4),
+                "f1_score": round(f1_score,4)
+            }
+            ]
+
+        return ret
